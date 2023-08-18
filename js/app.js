0d620c49b9eba49c3fe666781a4ef308ac4dd845
Caleb-McKinney <McKinney.Caleb@gmail.com>
2023-08-17 20:51:06 -0500

    completed problem 3

--- js/app.js
+++ js/app.js
@@ -21,8 +21,8 @@ const num2 = 19;
 const resultArray = sum(num1, num2);
 console.log(resultArray);
 
-// Here is the test for sum(); uncomment it to run it
- testSum(4, 7);
+// // Here is the test for sum(); uncomment it to run it
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -36,12 +36,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) {
-  //eslint-disable-line
+  const product = a * b;
+  const resultString = `The product of ${a} and ${b} is ${product}.`;
+  return [product, resultString];
 }
 
-// Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+const a = 5;
+const b = 9;
+const [product, resultString] = multiply(a, b);
 
+console.log(resultString);
+console.log(product);
+//eslint-disable-line
+
+// Here is the test for multiply(); uncomment it to run it
+testMultiply(5, 9);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -57,11 +66,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
-  //eslint-disable-line
+  const sumNumb = sum(a, b);
+  const varForC = sum(sumNumb[0], c)[0];
+  const prodNum = multiply(a, b);
+  const prodForC = multiply(prodNum[0], c)[0];
+  const sumString = `${a} and ${b} and ${c} sum to ${varForC}.`
+  const productString = `The product of ${a} and ${b} and ${c} is ${prodForC}.`;
+  //console.log(varForC[0]); //eslint-disable-line
+  return [varForC, prodForC, sumString, productString];
 }
+sumAndMultiply(4, 7, 5);
+// console.log(sumAndMultiply(4, 7, 5));
+// sumAndMultiply(4, 7, 5);
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+//Here is the test for sumAndMultiply(); uncomment it to run it
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
